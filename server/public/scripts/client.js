088c61ee6e8a40fd9aa20a65e10692912a23b62c
Crystal Redmon <cato2848@gmail.com>
2022-10-07 14:03:13 -0500

    setup client.js

---
+++ server/public/scripts/client.js
@@ -0,0 +1,28 @@
+console.log('I am in client');
+
+$(document).ready(onReady);
+
+
+
+function onReady(){
+//click listeners
+
+//call GET task function
+
+};
+
+
+//function to get all tasks- GET
+
+
+//function to add task- POST
+
+
+
+//function to delete task- DELETE
+
+
+//function to complete task- PUT
+
+
+//render function
