b7d39db5bad045b0f4e409d750e0583a5ed3ffee
Crystal Redmon <cato2848@gmail.com>
2022-10-07 20:07:00 -0500

    complete client GET

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -8,12 +8,28 @@ function onReady(){
 //click listeners
 
 //call GET task function
-
+    getAllTasks();
 };
 
 
 //function to get all tasks- GET
+function getAllTasks(){
 
+    let newTask = $('#inputForm').val();
+    console.log(newTask);
+    
+    $.ajax({
+        method:'GET',
+        url: '/task'    
+    })
+    .then(response=> {
+        render(response);
+    })
+    .catch(error=>{
+        console.log('New task not added to list', error);
+    });
+
+};
 
 //function to add task- POST
 
