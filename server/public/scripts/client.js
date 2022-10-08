8e8172bfc94a1be7d7f2dfc58dbffccb6e4f31ba
Crystal Redmon <cato2848@gmail.com>
2022-10-07 21:06:33 -0500

    complete client/server POST

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,4 +1,8 @@
+// const tasksRouter = require("../../routes/router.tasks");
+
 console.log('I am in client');
+
+let allTasks 
 
 $(document).ready(onReady);
 
@@ -6,7 +10,7 @@ $(document).ready(onReady);
 
 function onReady(){
 //click listeners
-
+    $('body').on('click', onAddNewTask)
 //call GET task function
     getAllTasks();
 };
@@ -15,24 +19,44 @@ function onReady(){
 //function to get all tasks- GET
 function getAllTasks(){
 
-    let newTask = $('#inputForm').val();
-    console.log(newTask);
-    
     $.ajax({
         method:'GET',
-        url: '/task'    
+        url: '/tasks'
     })
     .then(response=> {
-        render(response);
+        allTasks = response;
+        console.log(allTasks);
+        render();
     })
     .catch(error=>{
-        console.log('New task not added to list', error);
+        console.log('Unable to get tasks', error);
     });
 
 };
 
 //function to add task- POST
+function onAddNewTask(evt){
 
+    evt.preventDefault();
+
+    //grab the input from the form 
+    let newTask = {
+        task: $('#inputForm').val()
+    }
+    
+    $.ajax({
+        method: 'POST',
+        url: '/tasks',
+        data: newTask
+    })
+    .then(response=>{
+        console.log(response);
+        getAllTasks();
+    })
+    .catch(error=>{
+        console.log("Error, new task not added", error)
+    })
+}
 
 
 //function to delete task- DELETE
@@ -42,3 +66,18 @@ function getAllTasks(){
 
 
 //render function
+
+function render(){
+
+    $('#tableBody').empty();
+
+    for(let task of allTasks){
+        $('#tableBody').append(`
+        <tr>
+        <td>${task.task}</td>
+        <td>${task.complete}</td>
+        </tr>
+        `)
+    };
+
+}
--- server/routes/router.tasks.js
+++ server/routes/router.tasks.js
@@ -1,8 +1,46 @@
+const { response } = require('express');
 const express = require('express');
 const tasksRouter = express.Router();
 const pool = require('../modules/pool');
 
 
+
+tasksRouter.get('/', (req, res)=>{
+    
+    pool.query(`SELECT * FROM "tasks";`)
+
+    .then(dbRes=>{
+        res.send(dbRes.rows);
+    })
+    .catch(error=>{
+        console.log('Unable to get tasks from DB', error);
+    });
+    
+});
+
+
+tasksRouter.post('/', (req, res)=>{
+    let newTask = req.body;
+    console.log(newTask.task);
+    
+
+    let sqlText = `INSERT INTO "tasks" ("task",  "complete")
+                    VALUES ($1, FALSE);`;
+
+    const sqlParams = [newTask.task]
+
+    pool.query(sqlText, sqlParams)
+        .then(response=>{
+            res.sendStatus(201);
+        })
+        .catch(error=>{
+            console.log(`Add task failed`, error);
+            res.send(500);
+        });
+
+
+
+});
 
 
 
